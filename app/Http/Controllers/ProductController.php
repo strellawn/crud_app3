567311b841703a6c07d18acc61c59531aa68ddc7
strellawn <vdanielly683@gmail.com>
2024-09-06 18:01:58 -0400

    errro dois

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -42,22 +42,35 @@ class ProductController extends Controller
 
     public function show(Product $product)
     {
-      
+        return view('products.show', compact ('product'));
     }
 
-    public function edit(string $id)
+    public function edit(Product $product)
     {
- 
+        return view('products.edit', compact('product'));
     }
 
-
-    public function update(Request $request, string $id)
+    public function update(Request $request, Product $product)
     {
+        $request->validate([
+            'nome_produto' => 'required',
+            'marca' => 'required',
+            'categoria' => 'required',
+            'valor_compra' => 'required|numeric',
+            'valor_venda' => 'required|numeric',
+            'qtd_estoque' => 'required|numeric',
+        ]);
+
+        $product ->update($request->all());
+        
+        return redirect()->route('products.index')->with('success', 'Produto atualizado com sucesso.');
 
     }
 
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
+        $product->delete();
 
+        return redirect()->route('products.index')->with('success', 'Produto excluído com sucesso.');
     }
 }
